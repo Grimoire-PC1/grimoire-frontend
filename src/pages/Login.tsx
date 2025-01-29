9128a3f2379dcdb6c38551478ad31598352b8d40
Emile Christie <emile.christie.pereira@ccc.ufcg.edu.br>
2025-01-29 19:34:11 -0300

    progresso na tela de login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,14 @@
-import { useEffect } from "react";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Form, Link, Navigate, useNavigate } from "react-router-dom";
 
 import { ENDPOINT } from "../constants/Endpoint"
 import { useUserStore } from "../stores/user/user.store";
 import { User } from "../interfaces/Models";
 import axiosInstace from "../services/axios";
 import LoginForm from "../components/LoginForm/LoginForm";
-import { HStack, Button, Separator, Card, Field, Input, Stack, Heading } from "@chakra-ui/react";
+import { HStack, Button, Separator, Card, Field, Input, Stack, Heading, ClientOnly, IconButton, Skeleton, Textarea } from "@chakra-ui/react";
+import { LuSun, LuMoon } from "react-icons/lu";
+import { useColorMode } from "@/components/ui/color-mode";
 
 export default function LoginPage() {
   const navigate = useNavigate();
@@ -39,6 +41,11 @@ export default function LoginPage() {
     }
   }, [navigate, setUser, user]);
 
+  const { toggleColorMode, colorMode } = useColorMode()
+
+  const [showSignInForm, setShowSignInForm] = useState(false);
+  const [showSignUpForm, setShowSignUpForm] = useState(false);
+
   if (user) {
     return (
       <Navigate
@@ -49,18 +56,55 @@ export default function LoginPage() {
   }
 
   return (
-    <>
-    <div className="w-1/2">
-      <Card.Root size="sm">
-        <Card.Header>
-          <Heading size="md"> Card - sm</Heading>
-        </Card.Header>
-        <Card.Body color="fg.muted">
-          This is the card body. Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit.
-        </Card.Body>
-      </Card.Root>
+    <div className="flex w-full h-full">
+      <div className="w-2/5 h-[100vh] bg-linear-to-r from-purple-950 to-slate-500">
+
+      </div>
+      <Separator orientation="vertical" size={"md"} height={"100"} />
+      <div className="w-3/5 padding">
+
+        <div className="text-center margin">
+          <span className="subtitle">START YOUR</span>
+          <p></p>
+          <span className="agreloy title">Grimoire</span>
+
+          <div className="text-left">
+            <h1 className="subtitle">ABOUT</h1>
+            <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus bibendum neque ac congue semper. Vestibulum pulvinar eros sit amet nisi eleifend feugiat. Aliquam tristique arcu metus, ac dapibus dui congue vitae. Curabitur consectetur, velit sed iaculis ullamcorper, sapien turpis ullamcorper metus, eget ultrices augue ipsum mattis felis.</p>
+          </div>
+
+          
+          {!showSignInForm ?
+            <div className="margin">
+            <Button onClick={()=>setShowSignInForm(true)} className="padding-xl margin-sides">Entrar</Button>
+            <Button className="padding-xl margin-sides">Cadastrar</Button>
+            </div>
+
+          :
+             <div className="margin">
+             <Form>
+             <Textarea resize="none" className="height" placeholder="e-mail" />
+             <br></br>
+             <Textarea resize="none" className="height" placeholder="senha" />
+             </Form>
+             <br></br>
+             <Button onClick={()=>setShowSignInForm(false)} className="padding-xl margin-sides">Entrar</Button>
+            </div> 
+
+          }
+
+        <div className="text-right right-bottom">
+          
+          <ClientOnly fallback={<Skeleton boxSize="8" />}>
+            <IconButton onClick={toggleColorMode} variant="outline" size="sm">
+              {colorMode === "light" ? <LuSun /> : <LuMoon />}
+            </IconButton>
+          </ClientOnly>
+          
+        </div>
+        </div>
+      </div>
+
     </div>
-    </>
   );
 }
