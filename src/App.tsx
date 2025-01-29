0006955bff2ef796fe78ac75cc052dc22c36edb7
Luiz Sergio Pompeu <luizsergiopompeu@gmail.com>
2025-01-29 07:53:29 -0300

    Mudanças no app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,46 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import { PublicRoutes } from "./routes/Routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useUserStore } from "./stores/user/user.store";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const user = useUserStore((state) => state.user);
+
+  const router = createBrowserRouter([
+    ...PublicRoutes,
+    {
+      path: "*",
+      element: (
+        <Navigate
+          to={
+            user ? (user.role === "COORDINATOR" ? "/tcc-match/dashboard" :
+                    user.orienteePaper ? "/tcc-match/papers" :
+                    "/tcc-match/home") : "/tcc-match/"
+          }
+          replace
+        />
+      ),
+    },
+  ]);
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 15000,
+      },
+    },
+  });
 
   return (
-    <>
-      <h2 className='text-2xl text-blue-500'>Hello World</h2>
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
