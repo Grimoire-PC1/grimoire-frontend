c0c22a8857d923ac13c4176ab17cf3acb865f9eb
Emile Christie Dantas Pereira <emile.christie.pereira@ccc.ufcg.edu.br>
2025-06-13 12:42:52 -0300

    desistindo do .env

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const api_url = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8082/" //URL_CONSTS.BACKEND //  "https://grimoire-backend/api/"; //
+const api_url = "http://150.165.111.156:8080/"; //import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8082/" //URL_CONSTS.BACKEND //  
 
 export const axiosInstace = axios.create({
   baseURL: api_url,
